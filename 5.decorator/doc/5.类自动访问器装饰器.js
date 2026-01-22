d3e5e78a2f175a4a5ec819bee72b992ad3f7546a
kilito <wudalong@3h1china.com>
2026-01-22 10:38:00 +0800

    类自动访问装饰器

--- 5.decorator/doc/5.类自动访问器装饰器.js
+++ 5.decorator/doc/5.类自动访问器装饰器.js
@@ -1,11 +1,46 @@
-class C {
-  #x = 1; // 使用私有字段存储值
-
+class Class {
+  #x = initializeX.call(this, 1);
   get x() {
     return this.#x;
   }
-
   set x(val) {
     this.#x = val;
   }
 }
+let { get: oldGet, set: oldSet } = Object.getOwnPropertyDescriptor(Class.prototype, "x");
+
+const logged = (value, context) => {
+  if (context.kind === "accessor") {
+    let { get, set } = value;
+    return {
+      get() {
+        console.log(`getting ${context.name}`);
+        return get.call(this);
+      },
+      set(val) {
+        console.log(`setting ${context.name} to ${val}`);
+        return set.call(this, val);
+      },
+      init(initialValue) {
+        console.log(`initializing ${context.name} with value ${initialValue}`);
+        return initialValue;
+      },
+    };
+  }
+};
+
+let {
+  get: newGet = oldGet,
+  set: newSet = oldSet,
+  init: initializeX = (initialValue) => initialValue,
+} = logged({ get: oldGet, set: oldSet }, { kind: "accessor", name: "x" }) ?? {};
+
+Object.defineProperty(Class.prototype, "x", { get: newGet, set: newSet });
+let clazz = new Class();
+// initializing x with value 1
+clazz.x;
+// getting x
+clazz.x = 123;
+// setting x to 123
+export {};
+
--- 5.decorator/doc/5.类自动访问器装饰器.ts
+++ 5.decorator/doc/5.类自动访问器装饰器.ts
@@ -1,3 +1,56 @@
-class C {
-  accessor x = 1;
+// 类自动访问器装饰器是一种新的类元素类型
+// 它在类的字段前添加 accessor 关键字
+// 自动访问器自动为字段创建 setter 和 getter方法，并将默认值存储在一个私有槽中
+
+type ClassAutoAccessorDecorator = (
+  value: {
+    get: () => unknown;
+    set(value: unknown);
+  },
+  context: {
+    kind: "accessor";
+    name: string | symbol;
+    access: { get(): unknown, set(value: unknown): void };
+    static: boolean;
+    private: boolean;
+    addInitializer(initializer: () => void): void;
+  }
+) => {
+  get?: () => unknown;
+  set?: (value: unknown) => void;
+  init?: (initialValue: unknown) => unknown;
+} | void;
+
+const logged = (value, context) => {
+  console.log('value', value); // { get: [Function: get x], set: [Function: set x] }
+  console.log('context', context);
+  if (context.kind === "accessor") {
+    let { get, set } = value;
+    return {
+      get() {
+        console.log(`getting ${context.name}`);
+        return get.call(this);
+      },
+      set(val) {
+        console.log(`setting ${context.name} to ${val}`);
+        return set.call(this, val);
+      },
+      init(initialValue) {
+        console.log(`initializing ${context.name} with value ${initialValue}`);
+        return initialValue;
+      }
+    };
+  }
 }
+
+class Class {
+  @logged accessor x = 1;
+}
+
+let clazz = new Class();
+// initializing x with value 1
+clazz.x;
+// getting x
+clazz.x = 123;
+// setting x to 123
+export { }
