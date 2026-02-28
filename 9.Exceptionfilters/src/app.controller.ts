b3e436d8b48bdab1673ac4150557f0bf0dbf2f59
kilito <wudalong@3h1china.com>
2026-02-28 09:48:57 +0800

    自定义异常

--- 9.Exceptionfilters/src/app.controller.ts
+++ 9.Exceptionfilters/src/app.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { ForbiddenException } from './forbidden.exception'; // 自定义异常
 
 @Controller()
 export class AppController {
   @Get('exception')
   exception() {
     // 当异常是未识别的（既不是 HttpException 也不是继承自 HttpException 的类）
-    // throw new Error('exception');
+    // throw new Error('未识别');
     // {"statusCode":500,"message":"Internal server error"}
 
     // throw new HttpException('Forbidden', HttpStatus.FORBIDDEN); 
@@ -18,4 +19,9 @@ export class AppController {
     }, HttpStatus.FORBIDDEN);
   }
   // {"status":403,"error":"This is a custom message"}
+
+  @Get('custom')
+  custom() {
+    throw new ForbiddenException();
+  }
 }
