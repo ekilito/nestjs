8f4ffd1ed5897efb6552697962a0c4b75316d7bb
kilito <wudalong@3h1china.com>
2026-03-05 15:28:21 +0800

    useGlobalInterceptors

--- 13.Interceptors/src/main.ts
+++ 13.Interceptors/src/main.ts
@@ -1,7 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from './app.module';
+
+import { Logging6Interceptor } from "./logger6.interceptor";
+import { Logging5Interceptor } from "./logger5.interceptor";
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+   app.useGlobalInterceptors(new Logging6Interceptor());
+   app.useGlobalInterceptors(new Logging5Interceptor());
+    await app.listen(3000);
 }
 bootstrap();
--- 13.Interceptors/src/pay.controller.ts
+++ 13.Interceptors/src/pay.controller.ts
@@ -1,21 +1,18 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
-import { Logging1Interceptor } from './logging1.interceptor';
-import { Logging2Interceptor } from './logging2.interceptor';
+import { Get, Controller, UseInterceptors } from "@nestjs/common";
+import { Logging1Interceptor } from "./logging1.interceptor";
+import { Logging2Interceptor } from "./logging2.interceptor";
+import { Logging3Interceptor } from "./logger3.interceptor";
+import { Logging4Interceptor } from "./logger4.interceptor";
 
 @Controller('pay')
-@UseInterceptors(Logging1Interceptor) // 第一个拦截器
-@UseInterceptors(Logging2Interceptor) // 第二个拦截器
+@UseInterceptors(Logging3Interceptor)
+@UseInterceptors(Logging4Interceptor)
 export class PayController {
   @Get()
+  @UseInterceptors(Logging1Interceptor)
+  @UseInterceptors(Logging2Interceptor)
   async pay() {
     console.log('pay...');
-    return 'pay';
+    return 'pay'
   }
 }
-/**
-  Before2...
-  Before1...
-  pay...
-  After1... 3ms
-  After2... 9ms
- */
