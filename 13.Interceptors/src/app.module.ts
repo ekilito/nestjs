d2fe5acb91c1c4918f24a63af51c477b8d427663
kilito <wudalong@3h1china.com>
2026-03-05 15:50:02 +0800

    provider中全局注册

--- 13.Interceptors/src/app.module.ts
+++ 13.Interceptors/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from "@nestjs/common";
 import { PayController } from './pay.controller';
-
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { Logging6Interceptor } from "./logger6.interceptor";
+import { Logging5Interceptor } from "./logger5.interceptor";
 @Module({
   controllers: [PayController],
-  providers: [],
-
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: Logging6Interceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: Logging5Interceptor,
+    }
+  ]
 })
 export class AppModule { }
