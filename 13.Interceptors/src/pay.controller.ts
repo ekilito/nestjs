6591603660cbccf4dc40bbef7b9f8deb5145cef0
kilito <wudalong@3h1china.com>
2026-03-05 16:53:23 +0800

    CacheInterceptor

--- 13.Interceptors/src/pay.controller.ts
+++ 13.Interceptors/src/pay.controller.ts
@@ -3,6 +3,7 @@ import { Logging1Interceptor } from "./logging1.interceptor";
 import { Logging2Interceptor } from "./logging2.interceptor";
 import { Logging3Interceptor } from "./logger3.interceptor";
 import { Logging4Interceptor } from "./logger4.interceptor";
+import { CacheInterceptor } from './cache.interceptor';
 
 @Controller('pay')
 @UseInterceptors(Logging3Interceptor)
@@ -27,5 +28,12 @@ export class PayController {
   async error() {
     console.log('error...');
     throw new BadRequestException('error');
+  }
+
+  @Get('cache')
+  @UseInterceptors(CacheInterceptor)
+  async cache() {
+    console.log('cache...');
+    return 'cache';
   }
 }
