4e1a06674f05721458a3980be31a12586d8ea8c5
kilito <wudalong@3h1china.com>
2026-03-06 11:41:40 +0800

    多个文件

--- 14.upload/src/upload.controller.ts
+++ 14.upload/src/upload.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Post, UseInterceptors, UploadedFile, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe, UploadedFiles } from '@nestjs/common';
-import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor, FileFieldsInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
 import { FileSizeValidationPipe } from './pipes/file-size-validation.pipe';
 @Controller('upload')
@@ -8,7 +8,7 @@ export class UploadController {
   index() {
     return { message: 'hello' }
   }
-  
+
   @Post('file')
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile(FileSizeValidationPipe) file: Express.Multer.File) {
@@ -35,5 +35,23 @@ export class UploadController {
   files(@UploadedFiles() files: Array<Express.Multer.File>) {
     console.log(files);
     return { message: 'Files uploaded successfully' };
+  }
+  // 多字段文件上传接口，允许同时上传不同类型的文件 
+  @Post('fileFields')
+  // FileFieldsInterceptor 的特点：
+  // 参数是一个配置数组，每个配置对象包含：name: 前端表单中的字段名， maxCount: 该字段允许上传的最大文件数量
+  @UseInterceptors(FileFieldsInterceptor([
+    { name: 'avatar', maxCount: 1 },
+    { name: 'background', maxCount: 1 },
+  ]))
+
+  // @UploadedFiles(): 获取所有上传的文件 
+  // 该方法返回一个对象，对象的每个属性对应前端表单中的一个字段名，属性值是一个文件数组
+  fileFields(@UploadedFiles() files: {
+    avatar?: Express.Multer.File[], // 头像文件数组（因为 maxCount=1，所以数组长度最多为 1）
+    background?: Express.Multer.File[] // 背景文件数组（因为 maxCount=1，所以数组长度最多为 1）
+  }) {
+    console.log(files);
+    return { message: 'successfully' };
   }
 }
