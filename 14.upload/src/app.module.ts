9e55d51960ad87b1c7e8aee4bca0c09e8798f3dc
kilito <wudalong@3h1china.com>
2026-03-06 14:12:12 +0800

    upload默认配置

--- 14.upload/src/app.module.ts
+++ 14.upload/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from "@nestjs/common";
 import { UploadController } from './upload.controller';
+import { MulterModule } from '@nestjs/platform-express'; // Multer 文件上传模块
 
 @Module({
-  controllers: [UploadController],
-
+  imports: [
+    MulterModule.register({ // 注册 Multer 模块的全局配置
+      dest: './upload', // 设置文件上传的默认存储目录为项目根目录下的 upload 文件夹
+    }),
+  ],
+  controllers: [UploadController]
 })
 export class AppModule { }
