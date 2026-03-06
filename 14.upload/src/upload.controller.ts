e1e5c729e96b49b81a0498f48670cd47e6e9f6f8
kilito <wudalong@3h1china.com>
2026-03-06 10:30:38 +0800

    ParseFilePipe

--- 14.upload/src/upload.controller.ts
+++ 14.upload/src/upload.controller.ts
@@ -1,21 +1,35 @@
-import { Controller, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express'; // FileInterceptor 专门处理文件上传的拦截器
+import { Controller, Post, UseInterceptors, UploadedFile, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
 import { FileSizeValidationPipe } from './pipes/file-size-validation.pipe';
 
 @Controller('upload')
 export class UploadController {
   @Post('file')
-  // 使用 FileInterceptor 拦截器处理文件上传 
-  // 参数 'file' 指定了前端表单中文件字段的名称 
-  // 该拦截器会自动处理 multipart/form-data 格式的请求
   @UseInterceptors(FileInterceptor('file'))
-  // UploadedFile ：获取上传文件的参数装饰器
-  // 参数类型为 Express.Multer.File，这是 Multer 库的文件类型定义
-  // file 参数包含上传文件的详细信息
-  // FileSizeValidationPipe ：文件大小验证管道，用于验证上传文件的大小是否超过 1MB
   uploadFile(@UploadedFile(FileSizeValidationPipe) file: Express.Multer.File) {
     console.log(file);
     return { message: 'File uploaded successfully' };
   }
+
+  @Post('file-validator')
+  @UseInterceptors(FileInterceptor('file'))
+  fileValidator(
+    // 上传文件的参数装饰器
+    // 第一个参数是 ParseFilePipe 实例，用于验证上传文件的大小和类型
+    // 第二个参数是文件字段的名称，这里是 'file'
+    @UploadedFile(
+      // ParseFilePipe 是一个文件验证管道，它会在文件被接收后、进入控制器方法前进行验证。如果验证失败，会自动返回相应的错误响应。
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 }), // 限制上传文件的最大大小
+          new FileTypeValidator({ fileType: 'image/png' }), // 限制上传文件的 MIME 类型
+        ],
+      }),
+    )
+    file: Express.Multer.File) {
+    console.log(file);
+    return { message: 'fileValidator' };
+  }
+
 }
